7238a4311ec811f27414025b2041ef8b1c6010c7
Katia <pysaryna@gmail.com>
2019-11-05 20:55:31 +0200

    Add CRM system

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$(".header__menu ul li:nth-child(1) a").addClass("active"),$(".item__features").mouseenter(function(){$(this).children().eq(1).fadeIn()}).mouseleave(function(){$(this).children().eq(1).fadeOut()}),$(".content__filter").click(function(){$(".filter__list").hasClass("visible")?($(".filter__list").removeClass("visible"),$(".filter__list").fadeOut()):($(".filter__list").addClass("visible"),$(".filter__list").fadeIn())})});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+    $('.header__menu ul li:nth-child(1) a').addClass('active');
+
+    $('.item__features')
+        .mouseenter(function () {
+            $(this).children().eq(1).fadeIn();
+        })
+        .mouseleave(function () {
+            $(this).children().eq(1).fadeOut();
+        });
+
+
+    $('.content__filter').click(function () {
+        if (!($('.filter__list').hasClass('visible'))) {
+            $('.filter__list').addClass('visible');
+            $('.filter__list').fadeIn();
+        } else {
+            $('.filter__list').removeClass('visible');
+            $('.filter__list').fadeOut();
+        }
+    });
 
 });
